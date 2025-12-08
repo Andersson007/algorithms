92e21f7411cc8ae69aaf41965d5164e60233f08b
Andrew Klychkov <andrew.a.klychkov@gmail.com>
2025-12-08 07:45:03 +0100

    Merge sort

--- sorting.py
+++ sorting.py
@@ -43,9 +43,36 @@ def quick_sort(nums):
     return quick_sort(left) + mid + quick_sort(right)
 
 
+# Time O(N log N)
+# Space O(N)
 def merge_sort(nums):
 
-    return nums
+    if len(nums) <= 1:
+        return nums
+
+    mid = len(nums) // 2
+    left = merge_sort(nums[:mid])
+    right = merge_sort(nums[mid:])
+
+    return merge(left, right)
+
+
+def merge(left, right):
+    result = []
+    i = 0
+    j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    result.extend(left[i:])
+    result.extend(right[j:])
+    return result
 
 
 print(bubble_sort([8, 10 , 1, 2, 25, 5, 15, 0]))
