a4bb9c7c3d40bc064c636243b32936fe4e496b06
Andrew Klychkov <andrew.a.klychkov@gmail.com>
2025-12-16 08:10:07 +0100

    Product Sum improve

--- product_sum.py
+++ product_sum.py
@@ -30,3 +30,48 @@ def product_sum(arr, depth=1):
 
 
 print(product_sum(arr))
+
+
+# Time O(N): as each element is visited only once.
+# Space O(D): where D is maximun nested depth
+#   of the input list. Space is used by the
+#   recursion call stack.
+#   Poses the stack overflow risk.
+def product_sum_rec(nums, depth):
+    total = 0
+    for elem in nums:
+        if isinstance(elem, list):
+            total += product_sum_rec(elem, depth + 1)
+
+        else:
+            total += elem
+
+    return total * depth
+
+
+# Time O(N)
+# Space O(D): where D is max stack (list/array) size.
+#  Both the recursive and iterative solutions use O(D) space,
+#  where D is the maximum nesting depth.
+#  However, here the dynemic memory (heap) is used.
+def product_sum_iter(nums):
+    stack = [(nums, 1)]
+    total = 0
+
+    while stack:
+        current, depth = stack.pop()
+        subtotal = 0
+
+        for elem in current:
+            if isinstance(elem, list):
+                stack.append((elem, depth + 1))
+            else:
+                subtotal += elem
+
+        total += subtotal * depth
+
+    return total
+
+
+print(product_sum_rec([1, 2, [3, 4], 5], 1))
+print(product_sum_iter([1, 2, [3, 4], 5]))
