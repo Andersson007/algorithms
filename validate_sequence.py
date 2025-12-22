1f22f0d784998732cbce9f5e7226ee65b2477f3e
Andrew Klychkov <andrew.a.klychkov@gmail.com>
2025-12-22 07:24:36 +0100

    Validate Sequence: add a simpler solution

--- validate_sequence.py
+++ validate_sequence.py
@@ -25,3 +25,31 @@ def validate_sequence(array, sequence):
     # If we are at the end of the sequence after traversing,
     # it means all its elements are in the array.
     return seq_idx == len(sequence)
+
+# Simpler solution.
+# 1. Walk through the main array once
+# 2. Keep a pointer into sequence
+# 3. When elements match, move the pointer forward
+# 4. If you reach the end of sequence, it’s a valid subsequence
+
+array1 = [2, 3, 5, 10, 4, 8, 9]
+seq1 = [2, 10, 8]  # is a subsequence
+seq2 = [-1, 5, 11]  # is not a subsequence
+
+# Time O(N)
+# Space O(1)
+def validate_seq(array, sequence):
+    seq_idx = 0
+
+    for val in array:
+        if seq_idx == len(sequence):
+            break
+
+        if val == sequence[seq_idx]:
+            seq_idx += 1
+
+    return seq_idx == len(sequence)
+
+
+print(validate_seq(array1, seq1))
+print(validate_seq(array1, seq2))
