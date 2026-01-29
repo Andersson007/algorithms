4c3306105323d2902a01cd4083fc5fbfef0f8265
Andrew Klychkov <andrew.a.klychkov@gmail.com>
2026-01-29 07:55:45 +0100

    tandem_bicycle.py: improve

--- tandem_bicycle.py
+++ tandem_bicycle.py
@@ -34,3 +34,26 @@ def tandem_bicycles(rider1_speeds, rider2_speeds, fastest):
 print(tandem_bicycles(rider1_speeds, rider2_speeds, True))
 # Min possible speed
 print(tandem_bicycles(rider1_speeds, rider2_speeds, False))
+
+# My solution
+rider1_speeds = [5, 5, 3, 9, 2]
+rider2_speeds = [3, 6, 7, 2, 1]
+
+# Time O(N log N)
+# Space O(1)
+def tandem_bicycle(rider1_speeds, rider2_speeds, fastest):
+    rider1_speeds.sort()
+    if fastest:
+        rider2_speeds.sort(reverse=True)
+    else:
+        rider2_speeds.sort()
+
+    total_speed = 0
+    for i in range(len(rider1_speeds)):
+        total_speed += max(rider1_speeds[i], rider2_speeds[i])
+
+    return total_speed
+
+
+print(tandem_bicycle(rider1_speeds, rider2_speeds, True))
+print(tandem_bicycle(rider1_speeds, rider2_speeds, False))
